3f7b6e96d745cb969b9be1fba862a15b93794226
Jamie Lendrum <jalendrum@gmail.com>
2021-08-27 17:07:30 +0100

    Fix R CMD CHECK errors

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,7 @@
 # Stop the NOTES from R CMD CHECK when creating columns with mutate()
 if(getRversion() >= "2.15.1")  
-  utils::globalVariables(c("level", "record", "tag", "value"))
+  utils::globalVariables(c("level", "record", "tag", "value","new_sr","sr_no",".","xref.y",
+                           "xref","yob.y","yod.y","new_sub","subrecord_no","data","tmp"))
 
 
 
